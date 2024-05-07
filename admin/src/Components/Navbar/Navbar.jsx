aac2757fff328e62ca53e03f11cfe5ac95f7e03b
RyanRanjith <kingranjith4@gmail.com>
2024-05-06 18:11:08 -0700

    a

---
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src= alt="" />
+      
+    </div>
+  )
+}
+
+export default Navbar
