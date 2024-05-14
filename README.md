fdbb72189f1075bec1607089f40ad57c9ea60de9
RyanRanjith <kingranjith4@gmail.com>
2024-05-14 08:40:11 -0700

    a

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # E-Commerce
 # to run admin - npm run dev 
 # update
+# backend -- node ./index.js
--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -29,7 +29,27 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append('product',image);
 
-    await fetch('http://')
+    await fetch('http://localhost:4000/upload',{
+      method:'POST',
+      headers:{
+        Accept:'application/json',
+      },
+      body:formData,
+    }).then((resp)=>resp.json()).then((data)=>{responseData=data});
+
+    if(responseData.success)
+      {
+       product.image = responseData.image_url;
+       console.log(product);
+       await fetch('http://localhost:4000/addproduct',{
+        method:'POST',
+        headers:{
+          Accept:'application/json',
+          'Content-Type':'application/json',
+        },
+        body:JSON.stringify(product),
+       })
+       }    
 
   }
 
