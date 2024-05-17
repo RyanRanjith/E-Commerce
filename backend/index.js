d2b3234f6039218d570e5dded7844e77c102d061
RyanRanjith <kingranjith4@gmail.com>
2024-05-17 08:52:15 -0700

    a

--- backend/index.js
+++ backend/index.js
@@ -181,7 +181,31 @@ app.post('/signup',async (req,res)=>{
     }
 
     const token = jwt.sign(data,'secret_ecom');
-    res.json({success:true,})
+    res.json({success:true,token})
+})
+
+// Creating endpoint for user login
+
+app.post('/login',async (req,res)=>{
+   let user = await Users.findOne({email:req.body.email});
+   if (user) {
+    const passCompare = req.body.password === user.password;
+    if (passCompare) {
+        const data = {
+            user:{
+                id:user.id
+            }
+        }
+        const token = jwt.sign(data,'secret_ecom');
+        res.json({success:true,token});
+    }
+    else{
+        res.json({success:false,errors:"Wrong PAssword"});
+    }
+   }
+   else{
+    res.json({success:false,errors:"Wrong Email Id"})
+   }
 })
 
 app.listen(port,(error)=>{
