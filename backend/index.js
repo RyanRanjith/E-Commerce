fe91da7cd4f474fbf17a34dcf4d41f4783110719
RyanRanjith <kingranjith4@gmail.com>
2024-05-30 08:05:41 -0700

    a

--- backend/index.js
+++ backend/index.js
@@ -221,7 +221,16 @@ app.get('/newcollections',async (req,res)=>{
 // creating endpoint for popular in women  section
 
 app.get('/popularinwomen',async (req,res)=>{
-    let products = await Product.find({category:""})
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched");
+    res.send(popular_in_women);
+})
+
+// creating endpoint fro adding products in cartdata
+
+app.post('/addtocart',async (req,res)=>{
+    console.log(req.body);
 })
 
 app.listen(port,(error)=>{
